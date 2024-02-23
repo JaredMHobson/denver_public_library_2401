4778fcc9ad359c023d7cc407286299ee26f84300
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 20:00:03 -0800

    Add checkout, return and checked_out? methods and checked_out attr in book

--- lib/book.rb
+++ lib/book.rb
@@ -6,6 +6,7 @@ class Book
     @first_name = details[:author_first_name]
     @last_name = details[:author_last_name]
     @publication_date = details[:publication_date]
+    @checked_out = false
   end
 
   def publication_year
@@ -14,5 +15,17 @@ class Book
 
   def author
     @first_name + " " + @last_name
+  end
+
+  def checkout
+    @checked_out = true
+  end
+
+  def return
+    @checked_out = false
+  end
+
+  def checked_out?
+    @checked_out
   end
 end
