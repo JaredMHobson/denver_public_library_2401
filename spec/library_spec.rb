6a38f8afb494672282adb29a724e1a9fe0448d99
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:39:33 -0800

    Add test for add_author method in library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,6 +5,7 @@ require './lib/library'
 RSpec.describe 'Library' do
   let (:book) { Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"}) }
   let (:charlotte_bronte) { Author.new({first_name: "Charlotte", last_name: "Bronte"}) }
+  let (:harper_lee) { Author.new({first_name: "Harper", last_name: "Lee"}) }
   let (:dpl) { Library.new("Denver Public Library") }
 
   describe '#initialize' do
@@ -18,6 +19,23 @@ RSpec.describe 'Library' do
 
     it 'has no authors by default' do
       expect(dpl.authors).to be_empty
+    end
+  end
+
+  describe '#add_author' do
+    it 'can add authors to its authors array' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+      dpl.add_author(charlotte_bronte)
+
+      expect(dpl.authors).to eq([charlotte_bronte])
+
+      dpl.add_author(harper_lee)
+
+      expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
     end
   end
 end
