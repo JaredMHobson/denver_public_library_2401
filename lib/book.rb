1ce8f555854999939a1318d1927e2102f7a1889d
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:03:44 -0800

    Add method to read full author name and publication_year in book

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,18 @@
 class Book
-  attr_reader :title, :author
+  attr_reader :title
 
   def initialize(details)
     @title = details[:title]
-    @author = details[:author]
+    @first_name = details[:author_first_name]
+    @last_name = details[:author_last_name]
     @publication_date = details[:publication_date]
+  end
+
+  def publication_year
+    @publication_date[-4..-1]
+  end
+
+  def author
+    @first_name + " " + @last_name
   end
 end
