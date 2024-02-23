56f3c1f290879352a8c1a0edc078e6309405e8d0
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:52:56 -0800

    Add test for books attribute in library

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -38,4 +38,21 @@ RSpec.describe 'Library' do
       expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
     end
   end
+
+  describe '#books' do
+    it 'can list all books by all authors added to the library' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      
+      dpl.add_author(charlotte_bronte)
+      
+      expect(dpl.books).to eq([jane_eyre, professor, villette])
+      
+      dpl.add_author(harper_lee)
+      
+      expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
+    end
+  end
 end
