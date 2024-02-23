76d822c1bb98c9409fa981205c75ea6f860bc3cb
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:30:47 -0800

    Add write method in author

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_reader :books
-  
+
   def initialize(details)
     @first_name = details[:first_name]
     @last_name = details[:last_name]
@@ -9,5 +9,18 @@ class Author
 
   def name
     @first_name + " " + @last_name
+  end
+
+  def write(book_title, pub_date)
+    book = Book.new(
+      {
+      title: book_title, 
+      publication_date: pub_date,
+      author_first_name: @first_name,
+      author_last_name: @last_name
+    }
+    )
+    @books << book
+    book
   end
 end
