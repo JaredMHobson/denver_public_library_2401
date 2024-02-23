0fb726f533276bc7fc4e33b4d3d1c3a77dd9f6a5
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:56:46 -0800

    Add add_authors_books helper method and update add_author method with it in library

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,20 @@
 class Library
-  attr_reader :name, :authors
+  attr_reader :name, :authors, :books
 
   def initialize(name)
     @name = name
     @authors = []
+    @books = []
   end
 
   def add_author(author)
     @authors << author
+    add_authors_books(author)
+  end
+
+  def add_authors_books(author)
+    author.books.each do |book|
+      @books << book
+    end
   end
 end
