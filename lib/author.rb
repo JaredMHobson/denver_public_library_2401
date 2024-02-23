ac928a390f99c03740ec5fa1c72f2247ddbac343
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:11:18 -0800

    Add books attribute and attr_reader in author

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,10 @@
 class Author
+  attr_reader :books
+  
   def initialize(details)
     @first_name = details[:first_name]
     @last_name = details[:last_name]
+    @books = []
   end
 
   def name
