0fdf27a4cfe9587d9f71a21677fc497ebd904a79
Jared Hobson <jaredmhobson@gmail.com>
2024-02-22 18:35:32 -0800

    Add test for name and authors methods in library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe 'Library' do
     it 'exists' do
       expect(dpl).to be_instance_of(Library)
     end
+
+    it 'has a name' do
+      expect(dpl.name).to eq("Denver Public Library")
+    end
+
+    it 'has no authors by default' do
+      expect(dpl.authors).to be_empty
+    end
   end
 end
